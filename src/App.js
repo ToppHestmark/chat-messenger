37faf4d580343da0cce785a0dafe22645cb3d4e1
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-27 10:54:48 +0200

    imported form from material-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import { Button, Input, InputLabel } from '@material-ui/core';
+import React, { useState } from 'react';
 import './App.css';
+import { FormControl } from '@material-ui/core';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('')
+  }
+
   return (
     <div className="app">
       <h1>Hello my ninja warriors</h1>
+
+      <form>
+      <FormControl>
+        <InputLabel>Enter a message...</InputLabel>
+        <Input 
+          value={input} 
+          onChange={event => setInput(event.target.value)} />
+        <Button 
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit" 
+          onClick={sendMessage}>
+          Send Message
+        </Button>
+      </FormControl>
+      </form>
+
+      {
+        messages.map(message => (
+          <p> {message} </p>
+        ))
+      }
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Wj5EaciOuyb6IVjQRQ-bg0go9vG3Lns",
+  authDomain: "facebook-messenger-49480.firebaseapp.com",
+  databaseURL: "https://facebook-messenger-49480.firebaseio.com",
+  projectId: "facebook-messenger-49480",
+  storageBucket: "facebook-messenger-49480.appspot.com",
+  messagingSenderId: "626478811696",
+  appId: "1:626478811696:web:73727ceb27acdb9acdbbf5",
+  measurementId: "G-2RQV7MC18W"
+};
