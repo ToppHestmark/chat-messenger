56fdf74010e936fc65962600a6dcb200b6531acf
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-27 11:41:06 +0200

    created message card in the Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,20 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import React from 'react';
 import './Message.css';
 
 function Message(props) {
   return (
-    <div className="message">
-      <h2>{props.username}: {props.text}</h2>
-    </div>
+      <Card className="message__card">
+        <CardContent>
+          <Typography
+            color="white"
+            variant="h5"
+            component="h2"
+            >
+            {props.username}: {props.text}
+          </Typography>
+        </CardContent>
+      </Card>
   )
 }
 
