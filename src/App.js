e8cfc8637c72a5c7b1dc6f0a6dbf542e758b28f1
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-27 14:22:05 +0200

    set up firebase and integrated username, timestamp and messages for the app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { FormControl } from '@material-ui/core';
 import Message from './components/Message';
+import db from './firebase';
+import firebase  from 'firebase';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([
-    {username: "Admirchakee", text: "Hello"},
-    {username: "Eframos", text: "Hola"}
-  ]);
+  const [messages, setMessages] = useState([]);
   const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    db.collection('messages')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))
+    })
+  }, [])
 
   useEffect(() => {
     setUsername(prompt('Please enter your name'))
@@ -18,9 +25,16 @@ function App() {
 
   const sendMessage = (event) => {
     event.preventDefault();
-    setMessages([...messages, {username: username, text: input}]);
+
+    db.collection('messages')
+    .add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
     setInput('')
-  }
+  };
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
+import firebase from 'firebase';
 
-
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyC3Wj5EaciOuyb6IVjQRQ-bg0go9vG3Lns",
   authDomain: "facebook-messenger-49480.firebaseapp.com",
   databaseURL: "https://facebook-messenger-49480.firebaseio.com",
@@ -9,4 +9,8 @@ const firebaseConfig = {
   messagingSenderId: "626478811696",
   appId: "1:626478811696:web:73727ceb27acdb9acdbbf5",
   measurementId: "G-2RQV7MC18W"
-};
+});
+
+const db = firebaseApp.firestore();
+
+export default db;
